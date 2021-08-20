a8bfbd20b587beaa0dc5bf6b2476cb960cfddfb5
dearvetri1 <dearvetri@gmail.com>
2021-08-20 12:30:09 -0400

    Work im progress

--- instance.tf
+++ instance.tf
@@ -1,10 +1,4 @@
-provider "aws" {
-  access_key = "AKIAVUFSMMTFHOUDHKES"
-  secret_key = "v5YhRawiyUTcFJA2bokIwhUEBOO2wkuGx3MTjwwk"
-  region = "us-east-1"
-}
-
 resource "aws_instance" "example" {
-  ami = "ami-0133407e358cc1af0"
+  ami = "${lookup(var.AMIS,var.AWS_REGION)}"
   instance_type = "t2.micro"
 }
--- vars.tf
+++ vars.tf
@@ -3,3 +3,10 @@ variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
   default = "us-east-1"
 }
+
+variable "AMIS" {
+  type = map
+  default = {
+    us_east_1 = "ami-0133407e358cc1af0"
+  }
+}
