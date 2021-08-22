7083422eabbbe664e160e6fadfc8b111c6fea880
dearvetri1 <dearvetri@gmail.com>
2021-08-22 10:06:40 -0400

    Added output  code to security group

--- securitygroup.tf
+++ securitygroup.tf
@@ -5,6 +5,9 @@ data "aws_ip_ranges" "us-east-1-ec2" {
   regions  = ["us-east-1"]
 }
 
+output "ips" {
+  value = "${data.aws_ip_ranges.us-east-1-ec2.cidr_blocks}"
+}
 
 resource "aws_security_group" "from_us_east-1_sg" {
   name = "from_us_east-1_sg"
