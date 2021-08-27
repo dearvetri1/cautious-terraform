ac114293e3567e657a080a24dd4d941fdcf94c8b
dearvetri1 <dearvetri@gmail.com>
2021-08-26 22:29:27 -0400

    Before Packer Demo

--- datatest/main.tf
+++ datatest/main.tf
@@ -1,12 +1,11 @@
 //Amazon Linux 2 AMI (HVM), SSD Volume Type - ami-0443305dabd4be2bc (64-bit x86) / ami-0806cc3ac66515671 (64-bit Arm)
-data "aws_ami" "linux" {
+data "aws_ami" "test" {
+  owners = ["amazon"]
   most_recent = true
-
-  owners = ["029840945606"]
 
   filter {
     name = "name"
-    values = ["amzn2-ami-hvm-2.0.20210721.2-x86_64-gp2 - ami-*"]
+    values = ["amzn2-ami-hvm-2.0.20210721.2-x86_64-gp2"]
   }
 
   filter {
@@ -14,13 +13,8 @@ data "aws_ami" "linux" {
     values = ["hvm"]
   }
 
-  filter {
-    name = "root-device-type"
-    values = ["ebs"]
-  }
 }
 
-
-output "ami_ids" {
-  value = "${data.aws_ami.linux.id}"
+output "amiid" {
+  value = "${data.aws_ami.test.id}"
 }
